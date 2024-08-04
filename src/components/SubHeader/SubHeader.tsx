a94fe8f126dcab680ec34865953442850676d0aa
Jose Freitas <pg42872@alunos.uminho.pt>
2024-08-04 15:14:56 +0100

    Added content and content animation between Sub Headers

--- src/components/SubHeader/SubHeader.tsx
+++ src/components/SubHeader/SubHeader.tsx
@@ -10,6 +10,7 @@ interface Subheaderprops {
 }
 
 export default function SubHeader({ isSubHeaderOpen }: Subheaderprops) {
+  console.log(isSubHeaderOpen);
   return (
     <div id="subheader" className={styles.flexContainer}>
       <div
@@ -21,7 +22,48 @@ export default function SubHeader({ isSubHeaderOpen }: Subheaderprops) {
             ? styles.open
             : ""
         }`}
-      ></div>
+      >
+        <div className={styles.smallerFormattingContainer}>
+          <div
+            id="babysubcategories"
+            className={`${styles.subCategories} ${
+              !isSubHeaderOpen.baby ? styles.notShow : `${styles.show} ${styles.fade_in}`
+            }`}
+          >
+            <div>Prendedor de Chupetas</div>
+            <div>Chocalhos</div>
+            <div>Peluches Pequenos</div>
+            <div>Botinhas/Sapatinhos</div>
+            <div>Meias</div>
+          </div>
+          <div
+            id="flowerssubcategories"
+            className={`${styles.subCategories} ${
+              !isSubHeaderOpen.flowers ? styles.notShow : `${styles.show} ${styles.fade_in}`
+            }`}
+          >
+            <div>Tulipas</div>
+            <div>Rosas</div>
+            <div>Girassóis</div>
+            <div>Lavanda</div>
+            <div>Dálias</div>
+            <div>Gerberas</div>
+            <div>Margaridas</div>
+          </div>
+          <div
+            id="accessoriessubcategories"
+            className={`${styles.subCategories} ${
+              !isSubHeaderOpen.accessories ? styles.notShow : `${styles.show} ${styles.fade_in}`
+            }`}
+          ></div>
+          <div
+            id="angelssubcategories"
+            className={`${styles.subCategories} ${
+              !isSubHeaderOpen.angels ? styles.notShow : `${styles.show} ${styles.fade_in}`
+          }`}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }

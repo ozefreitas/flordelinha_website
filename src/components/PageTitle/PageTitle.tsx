8c38df0a911dadc882ec6b38f3b0dec734e63ce3
pg42872 <pg42872@alunos.uminho.pt>
2024-08-07 23:37:18 +0100

    Added animation based on location

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./pagetitle.module.css";
 import route_matches from "../../assets/pagename_match.json";
 
@@ -14,9 +15,11 @@ interface PageTitleProps {
   };
 }
 
-export default function PageTitle({ currentPage }: PageTitleProps) {
-  const [pageTitle, setPageTitle] = useState("");
+export default function PageTitle({ currentPage }: Readonly<PageTitleProps>) {
+  const [pageTitle, setPageTitle] = useState("Home");
+  const [animate, setAnimate] = useState(false);
 
+  const location = useLocation().pathname;
   useEffect(() => {
     Object.entries(currentPage).map(([pages, isActive]) => {
       if (isActive) {
@@ -24,15 +27,27 @@ export default function PageTitle({ currentPage }: PageTitleProps) {
           if (route.path === pages) {
             let newString = route.page[0].toUpperCase() + route.page.slice(1);
             setPageTitle(newString);
+            setAnimate(true);
           }
         });
       }
     });
-  }, [currentPage]);
+  }, [location]);
 
-  console.log(pageTitle);
+  useEffect(() => {
+    if (animate) {
+      const timer = setTimeout(() => setAnimate(false), 500); // Remove animation class after animation duration
+      return () => clearTimeout(timer);
+    }
+  }, [animate]);
+
   return (
-    <div id="pagetitle" className={styles.mainPageTitleContainer}>
+    <div
+      id="pagetitle"
+      className={`${styles.mainPageTitleContainer} ${
+        animate ? styles.animate : ""
+      }`}
+    >
       {pageTitle}
     </div>
   );

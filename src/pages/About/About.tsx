0d1be21f87a8e85cb158c4aa99acec35fad24dac
Jose Freitas <pg42872@alunos.uminho.pt>
2024-10-18 23:35:51 +0100

    [ADD] added the dummy data to the remainder of the pages

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,71 @@
 import styles from "./about.module.css"
+import { useState, useEffect } from "react";
+import AppearAsScroll from "../../components/AppearAsScroll/AppearAsScroll";
 
-export default function About() {
-    return <div></div>
+interface AboutProps {
+    windowHeight: number;
+  }
+
+export default function About({ windowHeight }: Readonly<AboutProps>) {
+    const [visible, setVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const scrollLimit = windowHeight >= 800 ? 250 : 200;
+    if (scrollPosition >= scrollLimit) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  const handleSlowScroll = (event: WheelEvent) => {
+    // Prevent default scroll behavior
+    event.preventDefault();
+
+    // Apply slower scrolling
+    window.scrollBy(0, event.deltaY * 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("wheel", handleSlowScroll, { passive: false });
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("wheel", handleSlowScroll);
+    };
+  });
+
+  return (
+    <div
+      id="about"
+      className={`${styles.mainAboutContainer} ${
+        visible ? styles.visible : styles.disapear
+      }`}
+    >
+      <AppearAsScroll>
+        <div style={{ height: "500px", backgroundColor: "#4caf50" }}>
+          <h1>Bem-Vind@ à Flor de Linha</h1>
+        </div>
+      </AppearAsScroll>
+      <AppearAsScroll>
+        <div style={{ height: "500px", backgroundColor: "red" }}>
+          <h1>
+            O objetivo desta loja é começar a ganhar algum para depois dar um
+            porsche ao meu namorado
+          </h1>
+        </div>
+      </AppearAsScroll>
+      <AppearAsScroll>
+        <div style={{ height: "1000px", backgroundColor: "yellow" }}>
+          <h1>
+            O objetivo desta loja é começar a ganhar algum para depois dar um
+            porsche ao meu namorado
+          </h1>
+        </div>
+      </AppearAsScroll>
+    </div>
+  );
 }
